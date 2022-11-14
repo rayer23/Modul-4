52e85117f1f2aafb50bca26686aad5d47c1739e8
Vrabe Eureka <brianuuuu@gmail.com>
2022-11-14 13:34:58 +0700

    first innit

---
+++ back-end/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const PORT = 2000;
+const server = express();
+const db = require("./models");
+const bearerToken = require("express-bearer-token");
+require("dotenv").config();
+
+server.use(express.json());
+server.use(cors());
+server.use(express.static("./Public"));
+server.use(bearerToken());
+
+// const { user } = require("./routers");
+// server.use("/users", user);
+
+server.listen(PORT, () => {
+  // db.sequelize.sync({ alter: true });
+  console.log("Success Running at PORT: " + PORT);
+});
